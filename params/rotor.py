553e2b1e9fb068a4a1d039450e55f010da8d9893
Mark Weng <mark.weng@abagile.com>
2020-06-04 21:41:48 +0800

    Optimize the params for 1kw

--- params/rotor.py
+++ params/rotor.py
@@ -4,7 +4,7 @@ rotor_params = {
     "Dri": "30mm",
     "pole": "10",
     "rotor_type": "1",
-    "mag_emb": "0.9",
+    "mag_emb": "0.8",
     "mag_thick": "3mm",
     "mag_width": "0mm",
     "rotor_bridge": "0mm",
