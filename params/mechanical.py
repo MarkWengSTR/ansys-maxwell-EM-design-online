06c2e13ba84e2354bd568d14b3650d9bf3fa9ce4
Mark Weng <mark.weng@abagile.com>
2020-06-05 17:14:09 +0800

    Rename stator rotor params to mechanical
    Because we will extend the calculation for this two params

--- params/mechanical.py
+++ params/mechanical.py
@@ -1,5 +1,18 @@
+rotor_params = {
+    "airgap": "0.5mm",
+    "Dro": "70mm",
+    "Dri": "30mm",
+    "pole": "10",
+    "rotor_type": "1",
+    "mag_emb": "0.8",
+    "mag_thick": "3mm",
+    "mag_width": "0mm",
+    "rotor_bridge": "0mm",
+    "rotor_rib": "0mm",
+}
+
 stator_params = {
-    "Dsi": "70mm",
+    "Dsi": "Dro + airgap*2",
     "Dso": "110mm",
     "slot": "12",
     "Hs0": "1.2mm",
@@ -13,3 +26,4 @@ stator_params = {
     "Bs2": "((Dsi + (Hs0 + Hs1 + Hs2)*2) *pi -12*Wt)/12",
     "slot_area": "((Bs1+Bs2)/1mm)/2*(Hs2/1mm)+((Bs0+Bs1)/1mm)/2*(Hs1/1mm)+(Bs2/1mm)*(Rs/1mm)",
 }
+
--- params/name.py
+++ params/name.py
@@ -1,4 +1,4 @@
-name_dirt = {
+name_params = {
     "band_name": ["rotaband", "outerband"],
     "excitation_name": ["N", "I_ph_A", "I_ph_B", "I_ph_C"]
 }
