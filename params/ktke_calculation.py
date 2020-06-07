1133b8d17c8423650c4dbff335ea9dbe71bb1318
Mark Weng <mark.weng@abagile.com>
2020-06-07 22:58:41 +0800

    Expend slot structure

--- params/ktke_calculation.py
+++ params/ktke_calculation.py
@@ -22,6 +22,7 @@ motor_cal_parms = {
         "slot": 12,
         "shoes_height_front": 1,
         "shoes_height_back": 1,
+        "slot_open": 4.5,
         "slot_corner_arc": 0.5,
     },
     "rotor": {
@@ -42,16 +43,19 @@ motor_cal_parms = {
         "conductor_OD": 1,
         "Y_para": 1,
         "membrane_ratio": 1.075,
+        "rate_J": 6,
+        "slot_fill_factor": 0.43,
     },
     "calculation": {
         "est_rotor_OD": None,
+        "est_stator_OD": None,
         "mag_thick": None,
         "teeth_width": None,
         "york_width": None,
         "slot_height": None,
         "slot_width_front": None,
         "slot_width_back": None,
-        "conductor_para": None,
+        "para_conductor": None,
         "coil_turns": None,
     },
 }
@@ -133,16 +137,70 @@ def expend_stator_teeth_york(total_cal_params):
     return total_cal_params
 
 
+def expend_stator_slot(total_cal_params):
+    cal_parms = total_cal_params["motor_cal_parms"]
+    stator = cal_parms["stator"]
+
+    stator_OD_limit = stator["OD_limit"]
+    conductor_OD = cal_parms["coil"]["conductor_OD"]
+    membrane_ratio = cal_parms["coil"]["membrane_ratio"]
+    coil_turns = cal_parms["calculation"]["coil_turns"]
+    slot = cal_parms["stator"]["slot"]
+    teeth_width = cal_parms["calculation"]["teeth_width"]
+    york_width = cal_parms["calculation"]["york_width"]
+    rotor_OD = cal_parms["calculation"]["est_rotor_OD"]
+    airgap = cal_parms["airgap"]
+    shoes_height_front = cal_parms["stator"]["shoes_height_front"]
+    shoes_height_back = cal_parms["stator"]["shoes_height_back"]
+
+    para_conductor = math.ceil(
+        total_cal_params["spec_params"]["current_rms"] /
+        (cal_parms["coil"]["rate_J"] * (conductor_OD ** 2 * math.pi / 4) * cal_parms["coil"]["Y_para"]))
+
+    coil_area = round(2 * coil_turns * ((conductor_OD *
+                                         membrane_ratio) ** 2 * math.pi / 4) * para_conductor, 2)
+
+    est_slot_area = round(coil_area / cal_parms["coil"]["slot_fill_factor"], 2)
+
+    max_slot_height = stator_OD_limit/2 - york_width - \
+        shoes_height_front - shoes_height_back - (rotor_OD + airgap*2)/2
+
+    slot_height = york_width  # for slot height & york 1:1
+
+    while slot_height < max_slot_height:
+        slot_width_front = (((rotor_OD + airgap*2) + (shoes_height_front +
+                                                      shoes_height_back)*2) * math.pi - slot * teeth_width) / slot
+
+        slot_width_back = (((rotor_OD + airgap*2) + (shoes_height_front +
+                                                     shoes_height_back + slot_height)*2) * math.pi - slot * teeth_width) / slot
+
+        real_slot_area = (slot_width_front + slot_width_back) / 2 * slot_height + \
+            (stator["slot_open"] + slot_width_front) / 2 * shoes_height_back + \
+            slot_width_back * stator["slot_corner_arc"]
+
+        if abs((real_slot_area - est_slot_area) / est_slot_area) < 0.01:
+            break
+        else:
+            slot_height += 0.01
+
+
+    cal_parms["calculation"]["para_conductor"] = para_conductor
+    cal_parms["calculation"]["est_stator_OD"] = (round(slot_height, 2) +  york_width + shoes_height_front + shoes_height_back + airgap)*2 + rotor_OD
+    cal_parms["calculation"]["slot_height"] = round(slot_height, 2)
+    cal_parms["calculation"]["slot_width_front"] = slot_width_front
+    cal_parms["calculation"]["slot_width_back"] = slot_width_back
+
+    return total_cal_params
+
+
 def mech_cal(total_cal_params):
     ktke_validate(total_cal_params) and \
         expend_NBLR(total_cal_params) and \
-        expend_stator_teeth_york(total_cal_params)
+        expend_stator_teeth_york(total_cal_params) and \
+        expend_stator_slot(total_cal_params)
 
     if total_cal_params["spec_params"]["error_present"]:
         raise BaseException(total_cal_params["spec_params"]["error_msg"])
-
-
-mech_cal(total_cal_params)
 
 # import ipdb; ipdb.set_trace()
 # ktke_validate(total_cal_params)
