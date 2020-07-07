e9a83d78d99d69bfb236aab76c3c336d5b1f4151
Mark Weng <mark.weng@abagile.com>
2020-07-07 21:42:42 +0800

    Separate run return straight and run at background and request other url

--- server_run_back.py
+++ server_run_back.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from threading import Thread
 from flask_cors import CORS
 from run import run_ansys
 from api.validate import spec_present, data_type_validate, spec_keys_validate, ansys_overload_check
+import requests
 ansys_processing_count = 0
 
 # debug
@@ -9,6 +11,21 @@ ansys_processing_count = 0
 
 app = Flask(__name__)
 CORS(app)  # local development cors
+
+class BackgroundProcess(Thread):
+    def __init__(self, ctx):
+        Thread.__init__(self)
+        self.ctx = ctx
+
+    def run(self):
+        ctx = run_ansys(self.ctx)
+
+        response = requests.post(ctx["request"]["res_url"], json=ctx["response"], headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
+
+        global ansys_processing_count
+        ansys_processing_count -= 1
+
+        print(response.status_code)
 
 @app.route('/run_simu', methods=["POST"])
 def run_simulation():
@@ -32,11 +49,12 @@ def run_simulation():
             data_type_validate(ctx) and \
             spec_keys_validate(ctx) and \
             ansys_overload_check(ctx):
-        ctx = run_ansys(self.ctx)
+        thread_a = BackgroundProcess(ctx)
+        thread_a.start()
     else:
         return jsonify(ctx["error"]["validate"])
 
-    return jsonify(ctx["response"])
+    return jsonify(ctx["start_run_response"])
 
 
 if __name__ == "__main__":
