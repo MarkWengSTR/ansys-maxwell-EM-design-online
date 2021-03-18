3aad0d7f919ddd827f0b5e26aad2ac289941a16d
MarkWengSTR <bskin0330@gmail.com>
2021-03-18 21:39:48 +0800

    initial distribute, add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+python_requires = [
+    "setuptools",
+    "pywin32==227",
+    "ramda==0.5.5",
+    "six==1.13.0",
+    "functional-pipeline==0.3.1",
+    "ipdb==0.12.3",
+    "Flask==1.1.2",
+    "Flask-Cors==3.0.8",
+    "pandas==1.0.1",
+    "numpy==1.18.1",
+    "requests==2.24.0",
+]
+
+setuptools.setup(
+    name="an_em_design",
+    version="0.1a",
+    author="Mark Weng",
+    author_email="bskin0330@gmail.com",
+    description="Electrical Machines Design Automation by Ansys Maxwell Script",
+    url="https://github.com/MarkWengSTR/ansys-maxwell-EM-design-online",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Windows > 7",
+    ],
+    python_requires=python_requires,
+)
